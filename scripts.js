66c569069a3ddf42f4b6542a631ed396174fb3b8
victorbdr <victor.ono.badaro@gmail.com>
2022-06-19 23:22:54 -0300

    cards number verification

--- scripts.js
+++ scripts.js
@@ -1,21 +1,37 @@
 
-  let howMany = Number(prompt("Com quantas cartas quer jogar?"));
-  function verify (){
-     if (howMany >= 4 && howMany <=12){
+  let cardsName=[
+    "bobross",
+    "explody",
+    "fiesta",
+    "metal",
+    "revertit",
+    "triplets",
+    "unicorn"
+  ]
+  function comparador() { 
+    return Math.random() - 0.5; 
+  }
+// Right number of cards verification
+  function verify (howMany){
+     if (Number(howMany) >= 4 && Number(howMany) <=14){
    return true;
  }
  false;
 }
+
 function isEven(howMany){
-if (howMany%2 == 0){
-    true;
+if (Number(howMany)%2===0){
+    return true;
 }
-false;
+return false;
+}
+
+function cartasCorretas (howMany){
+  return verify(howMany) && isEven(howMany);
 }
 function showCards(howMany){
-    if (!verify || !isEven){
-    return alert ("Vamos Jogar!")}
-    else{
-      return  alert ("ai nao!")
-    }
-}
+  while (howMany === null || !cartasCorretas(howMany)){
+  howMany= prompt("Digite um numero entre 4 e 14");
+  }
+   alert ("Vamos Jogar!!")
+  } 
